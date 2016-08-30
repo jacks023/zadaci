9b56b8919b3330f583e07f34a8cf6d77c237c779
Nikola <pesic.n023@gmail.com>
2016-08-30 14:49:22 +0200

    Zadaci

---
+++ c_to_f.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+
+float celsius,fahrenheit;
+printf("\nUnesite temperaturu u Celzijusima: ");
+scanf("%f",&celsius);
+
+fahrenheit = (1.8 * celsius) + 32;
+printf("Temperatura u Farenhaitima je: %.2f\n",fahrenheit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
