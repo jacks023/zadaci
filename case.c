361346156ca3e9e866f325840efeb34d45baa0d8
jacks023 <pesic.n023@gmail.com>
2016-09-02 14:25:49 +0200

    Formatiranje i funkcije

--- case.c
+++ case.c
@@ -2,46 +2,48 @@
 #include <string.h>
 #include <stdlib.h>
 
+konvert_cf (float celsius, float fahrenheit) {
+printf("\nUnesite temperaturu u Celzijusima: ");
+	scanf("%f", &celsius);
+	fahrenheit = (1.8 * celsius) + 32;
+	printf("Temperatura u Farenhaitima je: %f\n", fahrenheit);
+}
+konvert_fc (float celsius, float fahrenheit) {
+printf("\nUnesite temperaturu u Farenhaitima: ");
+	scanf("%f", &fahrenheit);
+	celsius = (5.0/9.0) * (fahrenheit-32);
+	printf("Temperatura u Celsiusima je: %f\n", celsius);
+}
 
 int main(void) {
-char izbor;
+int izbor=0;
 float celsius, fahrenheit;
 
 while(izbor !='3') {
-printf("Sta zelite da konvertujete?:\n");
-printf("\n----------------------------");
-printf("\n1. Iz Celzijusa u Farenhaite\n");
-printf("2. Iz Farenhaita u Celzijuse\n");
-printf("3. Izlaz\n");
-printf("Unesite vas izbor: ");
+	printf("Sta zelite da konvertujete?:\n");
+	printf("\n----------------------------");
+	printf("\n1. Iz Celzijusa u Farenhaite\n");
+	printf("2. Iz Farenhaita u Celzijuse\n");
+	printf("3. Izlaz\n");
+	printf("Unesite vas izbor: ");
 
-scanf("%c", &izbor);
-izbor = getchar();
+	izbor = getchar();
 
 switch (izbor) {
 case '1':
-printf("\nUnesite temperaturu u Celzijusima: ");
-scanf("%f", &celsius);
-fahrenheit = (1.8 * celsius) + 32;
-printf("Temperatura u Farenhaitima je: %f\n", fahrenheit);
-break;
+	konvert_cf( celsius, fahrenheit);
+	break;
 
 case '2':
-printf("\nUnesite temperaturu u Farenhaitima: ");
-scanf("%f", &fahrenheit);
-celsius = (5.0/9.0) * (fahrenheit-32);
-printf("Temperatura u Celsiusima je: %f\n", celsius);
-break;
+	konvert_fc( celsius, fahrenheit);
+	break;
 
 case '3':
-
-printf("Hvala na koristenju programa.\n");
-break;
+	printf("Hvala na koristenju programa.\n");
+	break;
 default:
-printf("Pogresan unos, pokusajte ponovo.\n");
-return main();
+	printf("Pogresan unos, pokusajte ponovo.\n");
 }
-
 (void)getchar();
 }
 return 0;
