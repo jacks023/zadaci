4b34dedc00ed3cadcfbec2d19a1dabfc020788e5
jacks023 <pesic.n023@gmail.com>
2016-09-03 14:56:07 +0200

    Funkcije

--- case.c
+++ case.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-void konvert_cf(float celsius1, float fahrenheit1) {
+void konvert_cf() {
   float celsius;
   float fahrenheit;
   printf("\nUnesite temperaturu u Celzijusima: ");
@@ -11,7 +11,7 @@ void konvert_cf(float celsius1, float fahrenheit1) {
   fahrenheit = (1.8 * celsius) + 32;
   printf("Temperatura u Farenhaitima je: %f\n", fahrenheit);
 }
-void konvert_fc(float celsius1, float fahrenheit1) {
+void konvert_fc() {
   float celsius;
   float fahrenheit;
   printf("\nUnesite temperaturu u Farenhaitima: ");
@@ -19,42 +19,47 @@ void konvert_fc(float celsius1, float fahrenheit1) {
   celsius = (5.0/9.0) * (fahrenheit-32);
   printf("Temperatura u Celsiusima je: %f\n", celsius);
 }
-void tabela(float celsius1, float fahrenheit1) {
+void tabela() {
   float celsius;
   float fahrenheit;
   printf("Celzijusi Farenhaiti\n");
+  printf("--------------------");
   celsius = 0;
   while (celsius <= 100) {
     fahrenheit = (1.8 * celsius) + 32;
-    printf("%.2f      %.2f\t\n", celsius, fahrenheit);
+    printf("\n%6.2f%11.2f", celsius, fahrenheit);
     celsius = celsius + 5; }
 }
 
-int main(celsius, fahrenheit) {
+int main() {
 
   int izbor = 0;
 
-  while (izbor !='4') {
-    printf("Sta zelite da konvertujete?:\n");
+  while (izbor !='5') {
+    printf("\nSta zelite da konvertujete?:");
     printf("\n----------------------------");
     printf("\n1. Iz Celzijusa u Farenhaite\n");
     printf("2. Iz Farenhaita u Celzijuse\n");
     printf("3. Tablica konvertovanja\n");
-    printf("4. Izlaz\n");
+    printf("4. Istorijat konvertovanja\n");
+    printf("5. Izlaz\n");
     printf("Unesite vas izbor: ");
 
     izbor = getchar();
 
     switch (izbor) {
       case '1':
-        konvert_cf(celsius, fahrenheit);
+        konvert_cf();
       break;
       case '2':
-        konvert_fc(celsius, fahrenheit);
+        konvert_fc();
       break;
       case '3':
-        tabela(celsius, fahrenheit);
+        tabela();
+      break;
       case '4':
+        printf("Istorijat konvertovanja\n");
+      case '5':
         printf("Hvala na koristenju programa.\n");
       break;
       default:
